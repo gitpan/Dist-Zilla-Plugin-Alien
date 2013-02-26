c1aabd84bea47d8bda29ae512d4e322c0f8cd7ed
Torsten Raudssus <torsten@raudssus.de>
2013-02-26 02:08:15 +0000

    Import of GETTY/Dist-Zilla-Plugin-Alien-0.001 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-Alien
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         GETTY/Dist-Zilla-Plugin-Alien-0.001.tar.gz
    gitpan-cpan-author:       GETTY
    gitpan-cpan-maturity:     released

---
+++ lib/Dist/Zilla/Plugin/Alien.pm
@@ -0,0 +1,157 @@
+package Dist::Zilla::Plugin::Alien;
+BEGIN {
+  $Dist::Zilla::Plugin::Alien::AUTHORITY = 'cpan:GETTY';
+}
+{
+  $Dist::Zilla::Plugin::Alien::VERSION = '0.001';
+}
+# ABSTRACT: Use Alien::Base with Dist::Zilla
+
+use Moose;
+extends 'Dist::Zilla::Plugin::ModuleBuild';
+with 'Dist::Zilla::Role::PrereqSource';
+
+use URI;
+
+
+has name => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_name {
+	my ( $self ) = @_;
+	my $name = $self->zilla->name;
+	$name =~ s/^Alien-//g;
+	return $name;
+}
+
+has repo => (
+	isa => 'Str',
+	is => 'rw',
+	required => 1,
+);
+
+has repo_uri => (
+	isa => 'URI',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_repo_uri {
+	my ( $self ) = @_;
+	URI->new($self->repo);
+}
+
+has pattern_prefix => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern_prefix {
+	my ( $self ) = @_;
+	return $self->name.'-';
+}
+
+has pattern_version => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern_version { '([\d\.]+)' }
+
+has pattern_suffix => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern_suffix { '\\.tar\\.gz' }
+
+has pattern => (
+	isa => 'Str',
+	is => 'rw',
+	lazy_build => 1,
+);
+sub _build_pattern {
+	my ( $self ) = @_;
+	join("",
+		$self->pattern_prefix.
+		$self->pattern_version.
+		$self->pattern_suffix
+	);
+}
+
+sub register_prereqs {
+	my ( $self ) = @_;
+	$self->zilla->register_prereqs({
+			type  => 'requires',
+			phase => 'configure',
+		},
+		'Alien::Base' => '0.002',
+	);
+	$self->zilla->register_prereqs({
+			type  => 'requires',
+			phase => 'runtime',
+		},
+		'Alien::Base' => '0.002',
+	);
+}
+
+has "+mb_class" => (
+	default => 'Alien::Base::ModuleBuild',
+);
+
+around module_build_args => sub {
+	my ($orig, $self, @args) = @_;
+	my $pattern = $self->pattern;
+	return {
+		%{ $self->$orig(@args) },
+		alien_name => $self->name,
+		alien_repository => {
+			protocol => $self->repo_uri->scheme,
+			host => $self->repo_uri->host_port,
+			location => $self->repo_uri->path,
+			pattern => qr/^$pattern$/,
+		},
+	};
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::Alien - Use Alien::Base with Dist::Zilla
+
+=head1 VERSION
+
+version 0.001
+
+=head1 DESCRIPTION
+
+  name = Alien-myapp
+
+  [Alien]
+  repo = http://ffmpeg.org/releases
+  name = myapp
+  pattern_prefix = myapp-
+  pattern_version = ([\d\.]+)
+  pattern_suffix = \.tar\.gz
+  pattern = myapp-([\d\.]+)\.tar\.gz
+
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudss.us>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Torsten Raudssus.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ lib/Dist/Zilla/PluginBundle/Alien.pm
@@ -0,0 +1,64 @@
+package Dist::Zilla::PluginBundle::Alien;
+BEGIN {
+  $Dist::Zilla::PluginBundle::Alien::AUTHORITY = 'cpan:GETTY';
+}
+{
+  $Dist::Zilla::PluginBundle::Alien::VERSION = '0.001';
+}
+# ABSTRACT: Dist::Zilla::PluginBundle::Basic for Alien
+
+use Moose;
+use Moose::Autobox;
+use Dist::Zilla;
+with 'Dist::Zilla::Role::PluginBundle::Easy';
+
+
+use Dist::Zilla::PluginBundle::Basic;
+
+sub configure {
+  my ($self) = @_;
+
+  $self->add_bundle('Filter' => {
+    -bundle => '@Basic',
+    -remove => ['MakeMaker'],
+  });
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::PluginBundle::Alien - Dist::Zilla::PluginBundle::Basic for Alien
+
+=head1 VERSION
+
+version 0.001
+
+=head1 DESCRIPTION
+
+This plugin bundle allows to use L<Dist::Zilla::Plugin::Alien> together
+with L<Dist::Zilla::PluginBundle::Basic>.
+
+  name = Alien-ffmpeg
+
+  [@Alien]
+  repo = http://ffmpeg.org/releases
+
+=head1 AUTHOR
+
+Torsten Raudssus <torsten@raudss.us>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Torsten Raudssus.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
